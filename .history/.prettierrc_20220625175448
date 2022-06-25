78e8a5e69ab042d584c171fb836d53ba1475d226
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-25 18:12:40 -0300

    chore: removed .history

--- .history/.prettierrc_20220625175448
+++
@@ -1,6 +0,0 @@
-// prettier.config.js or .prettierrc.js
-module.exports = {
-  trailingComma: 'es5',
-  semi: false,
-  singleQuote: true,
-};
--- .history/src/App_20220625175451.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return <h1>Hello World!</h1>;
-}
-
-export default App;
