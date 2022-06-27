7615ef7d7bc75b1999eb96090ba9ee9a76d1cea7
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-06-26 22:49:58 -0300

    refactor: changed to environment variables

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4uf8w3p0thf01unc9ox5v47/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache(),
 })
